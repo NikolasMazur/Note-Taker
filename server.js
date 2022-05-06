5e109ffd9089eda3ea385dbcbe52251c70bd6e62
Nikolas Mazur <kolemazur@gmail.com>
2022-05-06 17:24:39 -0600

    Add API and HTML routes

--- server.js
+++ server.js
@@ -3,8 +3,13 @@ const apiRoutes = require(routes/apiRoutes);
 const htmlRoutes = require(routes/htmlRoutes);
 const PORT = process.env.PORT || 3001;
 
+// Express
 const app = express();
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.search(express.static("public"));
+
+// Routes
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
