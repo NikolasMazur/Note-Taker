ae6fbe1c61fa71de2de43c800b5e5fa9156f66ea
Nikolas Mazur <kolemazur@gmail.com>
2022-05-07 01:30:11 -0600

    Add route

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,6 +2,10 @@ const path = require("path");
 const router = require("express").Router();
 
 // Note routes
-    router.get("/notes", (req, res) => {
-        res.sendFile(path.join(__dirname, "../public/notes.html"));
-    });
+router.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
+});
+
+router.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
