d5319be945cb3773cbc471f95d6fe38b6fd2c0dc
Nikolas Mazur <kolemazur@gmail.com>
2022-05-07 23:05:54 -0600

    Update routes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const apiRoutes = require(routes/apiRoutes);
-const htmlRoutes = require(routes/htmlRoutes);
+const apiRoutes = require("./routes/apiRoutes.js");
+const htmlRoutes = require("./routes/htmlRoutes.js");
 const PORT = process.env.PORT || 3001;
 
 // Express
@@ -15,4 +15,4 @@ app.use("/api", apiRoutes);
 app.use("/", htmlRoutes);
 
 // Port listener
-app.listen(PORT, () => console.log(`Listen on PORT: ${PORT}`));
+app.listen(PORT, () => console.log(`Listen on PORT: ${PORT}`, ));
