ec6c96746ae883da13f3e4083e48e15967351ab5
Nikolas Mazur <kolemazur@gmail.com>
2022-05-08 10:42:32 -0600

    Refactor HTML routings

--- server.js
+++ server.js
@@ -8,11 +8,17 @@ const app = express();
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
+app.use("/api", apiRoutes);
 app.search(express.static("public"));
 
-// Routes
-app.use("/api", apiRoutes);
-app.use("/", htmlRoutes);
+// HTML Routes
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+  });
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+  });
 
 // Port listener
-app.listen(PORT, () => console.log(`Listen on PORT: ${PORT}`, ));
+app.listen(PORT, () => console.log(`Listen on PORT: ${PORT}`));
