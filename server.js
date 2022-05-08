48c90074429db0d85893fb1572f8bf824d2c4c2f
Nikolas Mazur <kolemazur@gmail.com>
2022-05-08 11:25:45 -0600

    Add path requirement

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const apiRoutes = require("./routes/apiRoutes.js");
-const htmlRoutes = require("./routes/htmlRoutes.js");
+const path = require("path");
+// const apiRoutes = require("./routes/apiRoutes.js");
 const PORT = process.env.PORT || 3001;
 
 // Express
@@ -8,17 +8,17 @@ const app = express();
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
-app.use("/api", apiRoutes);
+// app.use("/api", apiRoutes);
 app.search(express.static("public"));
 
 // HTML Routes
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
-  });
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/notes.html"));
-  });
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
 
 // Port listener
 app.listen(PORT, () => console.log(`Listen on PORT: ${PORT}`));
