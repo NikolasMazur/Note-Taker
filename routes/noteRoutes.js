85b7cc72462a7bd4255b98084af8ca2c703ba6a5
Nikolas Mazur <kolemazur@gmail.com>
2022-05-10 09:25:32 -0600

    Add requirements

---
+++ routes/noteRoutes.js
@@ -0,0 +1,6 @@
+const router = require("express").Router();
+let data = require("../db/db.json");
+const { v4: uuidv4 } = require("uuid");
+const fs = require("fs");
+const path = require("path");
+
