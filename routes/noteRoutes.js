17311bfd2586e14e7667c156d260b9e6fb728120
Nikolas Mazur <kolemazur@gmail.com>
2022-05-10 09:50:39 -0600

    Add router

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -4,3 +4,9 @@ const { v4: uuidv4 } = require("uuid");
 const fs = require("fs");
 const path = require("path");
 
+// Get router
+router.get("/notes", (req, res) => {
+  console.log({ data });
+  res.json(data);
+});
+
