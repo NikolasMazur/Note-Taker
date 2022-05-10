4135d9da9d6083a63a4a1d5edbf5f7f0520bc92a
Nikolas Mazur <kolemazur@gmail.com>
2022-05-10 10:02:01 -0600

    Add notes save functionality

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -10,3 +10,20 @@ router.get("/notes", (req, res) => {
   res.json(data);
 });
 
+router.post("/notes", (req, res) => {
+  const newNote = { ...req.body, id: uuidv4() };
+  console.log(newNote);
+  console.log(req.body);
+  data.unshift(newNote);
+  fs.writeFile(
+    path.join(__dirname, "../db/db.json"),
+    JSON.stringify(data),
+    function (err) {
+      if (err) {
+        res.status(404).json({ error: err });
+      }
+      res.json(data);
+    }
+  );
+});
+
